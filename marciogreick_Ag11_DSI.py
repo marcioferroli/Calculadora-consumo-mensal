8adcb6194b6e7de875505093a82e775f24cef13e
Márcio Greick Ferreira de Oliveira <marcioferroli@hotmail.com>
2026-05-03 20:47:05 -0300

    atividade agenda 11

---
+++ marciogreick_Ag11_DSI.py
@@ -0,0 +1,25 @@
+from colorama import Fore, Style, init
+init(autoreset=True)
+niveis = [
+    "Muito baixo (crítico)",
+    "Baixo",
+    "Médio",
+    "Alto",
+    "Muito alto (alerta)"
+]
+def definir_cor(nivel):
+    if nivel == 0:
+        return Fore.RED
+    elif nivel == 1:
+        return Fore.YELLOW
+    elif nivel == 2:
+        return Fore.GREEN
+    elif nivel == 3:
+        return Fore.CYAN
+    elif nivel == 4:
+        return Fore.BLUE
+for i in range(len(niveis)):
+    cor = definir_cor(i)
+    print(cor + f"Nível {i+1}: {niveis[i]}")
+print(Style.RESET_ALL)
+print('depois do reset')
